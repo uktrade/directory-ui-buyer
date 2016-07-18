e6b18a5bb806f28146e39efdd0c4e0b7160ad35b
Graham Veal <grahamveal@gmail.com>
2016-07-18 15:48:58 +0100

    Added my styles and templates for the UI

--- ui/forms.py
+++ ui/forms.py
@@ -5,7 +5,7 @@ class ContactForm(forms.Form):
     company_name = forms.CharField(required=True, max_length=255)
     contact_name = forms.CharField(required=True, max_length=255)
     email_address1 = forms.EmailField(required=True, max_length=255)
-    email_address1 = forms.EmailField(required=True, max_length=255)
+    email_address2 = forms.EmailField(required=True, max_length=255)
     mobile_number = forms.CharField(required=False, max_length=255)
     office_number = forms.CharField(required=False, max_length=255)
     website = forms.CharField(required=False, max_length=255)
