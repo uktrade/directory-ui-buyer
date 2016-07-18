2d8e8743ce697b0ee17fbd316e1ccc24904fc3c1
Adam Malinowski <adam.malinowski@digital.bis.gov.uk>
2016-07-18 14:49:52 +0100

    bits

---
+++ ui/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    company_name = forms.CharField(required=True, max_length=255)
+    contact_name = forms.CharField(required=True, max_length=255)
+    email_address1 = forms.EmailField(required=True, max_length=255)
+    email_address1 = forms.EmailField(required=True, max_length=255)
+    mobile_number = forms.CharField(required=False, max_length=255)
+    office_number = forms.CharField(required=False, max_length=255)
+    website = forms.CharField(required=False, max_length=255)
+    address1 = forms.CharField(required=True, max_length=255)
+    address2 = forms.CharField(required=False, max_length=255)
+    town_or_city = forms.CharField(required=True, max_length=255)
+    county = forms.CharField(required=False, max_length=255)
+    postcode = forms.CharField(required=True, max_length=255)
+    exporting = forms.ChoiceField(
+        required=True,
+        choices=[('True', 'True'), ('False', 'False')],
+        widget=forms.RadioSelect(),
+    )
--- ui/views.py
+++ ui/views.py
@@ -2,12 +2,14 @@ import json
 
 from django.conf import settings
 from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import TemplateView, FormView
 
 from alice.helpers import rabbit
+from ui.forms import ContactForm
 
 
-class IndexView(TemplateView):
+class IndexViewOld(TemplateView):
     template_name = "index.html"
 
     def post(self, request):
@@ -23,3 +25,26 @@ class IndexView(TemplateView):
             return redirect("problem")
 
         return redirect("thanks")
+
+
+class IndexView(FormView):
+    template_name = "index.html"
+    form_class = ContactForm
+    success_url = reverse_lazy("thanks")
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+
+        r = self.request.POST
+        json_data = {'data': json.dumps(r)}
+        response = rabbit.post(
+            settings.DATA_SERVER + '/form/',
+            data=json_data,
+            request=self.request,
+        )
+
+        if not response.status_code == 201:
+            return redirect("problem")
+
+        return super().form_valid(form)
