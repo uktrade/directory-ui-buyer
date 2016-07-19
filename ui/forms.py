086e4f487a094ce21009adef726f50bd463e136f
Adam Malinowski <adam.malinowski@digital.bis.gov.uk>
2016-07-19 11:50:47 +0100

    email addresses match validation

--- ui/forms.py
+++ ui/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms.utils import ErrorList
 
 
 class ContactForm(forms.Form):
@@ -16,12 +17,16 @@ class ContactForm(forms.Form):
     agree_terms = forms.BooleanField(required=True)
     opt_in = forms.BooleanField(required=False)
 
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     email1 = cleaned_data.get("email_address1")
-    #     email2 = cleaned_data.get("email_address2")
+    def clean(self):
+        cleaned_data = super().clean()
+        email1 = cleaned_data.get("email_address1")
+        email2 = cleaned_data.get("email_address2")
 
-    #     if email1 and email2 and email1 != email2:
-    #         raise forms.ValidationError("Emails do not match.")
-
-    #     return cleaned_data
+        if email1 and email2 and email1 != email2:
+            for field in ['email_address1', 'email_address2']:
+                if field not in self._errors:
+                    self._errors[field] = ErrorList()
+                self._errors[field].append(
+                    'Email addresses do not match',
+                )
+        return cleaned_data
