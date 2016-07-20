21102526f6b0ffd9ca365367fad0d8bad2fbcfc7
Bowen Sun <bowensun@gmail.com>
2016-07-20 11:47:33 +0100

    build static assets into docker image

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM python:3.5-onbuild
 
+RUN python /usr/src/app/manage.py collectstatic
+
 CMD ["/usr/src/app/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
 
-python /usr/src/app/manage.py migrate
 gunicorn -c /usr/src/app/gunicorn/conf.py ui.wsgi --log-file - -b [::1]:8000 -b 0.0.0.0:8000
 
