5662f355aee094676049c1440bca516f3d5524ee
Bowen Sun <bowensun@gmail.com>
2016-07-20 12:29:13 +0100

    build static assets at docker run

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
-python /usr/src/app/manage.py collectstatic
+python /usr/src/app/manage.py collectstatic --noinput
 gunicorn -c /usr/src/app/gunicorn/conf.py ui.wsgi --log-file - -b [::1]:8000 -b 0.0.0.0:8000
 
