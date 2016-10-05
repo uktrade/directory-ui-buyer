b974172320168211520d46d9e563c7a773f462f9
Francisco Benedict <franciscobenedict@Franciscos-MacBook-Pro.local>
2016-10-05 15:58:18 +0100

    removing virtual env commit

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/franciscobenedict/OneDrive/contract_work/dit-uk-export-directory/directory-ui/.venv/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
