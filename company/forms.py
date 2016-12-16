8f7d8dae5821ba3d9d6b5dfbae9821b031bd82ca
Rich Tier <rikatee@gmail.com>
2016-12-16 13:21:05 +0000

    Update the content of the contact form page given feedback from Content

--- company/forms.py
+++ company/forms.py
@@ -184,24 +184,25 @@ class CompanyContactDetailsForm(AutoFocusFieldMixin,
     }
 
     email_full_name = forms.CharField(
-        label='Full name:',
+        label='Contact name for buyer enquiries:',
         max_length=200,
         help_text=(
-            'This is the full name that international buyers should use'
-            ' when contacting your company.'
+            'This is the full name of the person that international buyers '
+            'should use when contacting your company.'
         ),
     )
     email_address = forms.EmailField(
+        label='Contact email address',
         help_text=(
             'This is the email address that international buyers should use'
             ' when contacting your company.'
         ),
     )
     mobile_number = MobilePhoneNumberField(
-        label='Your mobile phone number:',
+        label='Your mobile phone number (optional):',
         required=False,
         help_text=(
-            'Optional. This is the phone number that international buyers'
+            'This is the phone number that international buyers'
             ' should use when contacting your company.'
         ),
     )
