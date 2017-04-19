7fcee235ee85453150ac2876ca617ebd5a2ca848
konradko <konrad@korzel.com>
2017-04-19 17:02:38 +0100

    Use consistent name for signature secret

--- sso/utils.py
+++ sso/utils.py
@@ -8,7 +8,7 @@ from directory_sso_api_client.client import DirectorySSOAPIClient
 
 sso_api_client = DirectorySSOAPIClient(
     base_url=settings.SSO_API_CLIENT_BASE_URL,
-    api_key=settings.SSO_API_CLIENT_KEY,
+    api_key=settings.SSO_SIGNATURE_SECRET,
 )
 
 
