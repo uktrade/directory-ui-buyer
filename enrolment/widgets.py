0256f57549e6377eeaff3a650516ae5a5fbe6766
Richard Tier <rihard@highertier.com>
2017-06-14 15:04:16 +0100

    Add custom renderer for checkboxes

---
+++ enrolment/widgets.py
@@ -0,0 +1,22 @@
+from django.utils.safestring import mark_safe
+from django.forms import widgets
+
+
+class CheckboxWithInlineLabel(widgets.CheckboxInput):
+    template =  """
+        <div class="form-field checkbox">
+            {input_html}
+            <label for="{id}">{label}</label>
+        </div>
+    """
+
+    def __init__(self, label, *args, **kwargs):
+        self.label = label
+        super().__init__(*args, **kwargs)
+
+    def render(self, name, value, attrs=None):
+        input_html = super().render(name, value, attrs=None)
+        wrapper_html = self.template.format(
+            input_html=input_html, label=self.label, id=attrs['id']
+        )
+        return mark_safe(wrapper_html)
