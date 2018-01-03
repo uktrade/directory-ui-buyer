6c0028e365efb640e2015a8f1e45b6ca629fd783
eadpearce <e.pearce@tutanota.com>
2018-01-03 11:11:11 +0000

    Added gulp sass, renamed partial or unused sass files

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+const path = require('path');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+const PROJECT_DIR = path.resolve(__dirname);
+const SASS_FILES = `${PROJECT_DIR}/enrolment/static/sass/*.scss`;
+const CSS_DIR = `${PROJECT_DIR}/enrolment/static`;
+
+gulp.task('sass', function () {
+  return gulp.src(SASS_FILES)
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write('.', {includeContent: false}))
+    .pipe(gulp.dest(CSS_DIR));
+});
+
+gulp.task('default', ['sass']);
