f5936bb4179ca7aa4d78cad4ed0dd36a5c590a82
gatwal <gurdeep.atwal@digital.trade.gov.uk>
2019-02-15 14:06:39 +0000

    fixed flake8

--- conf/settings.py
+++ conf/settings.py
@@ -417,4 +417,3 @@ RESTRICTED_APP_NAMES = env.list(
 if env.bool('IP_RESTRICTOR_RESTRICT_UI', False):
     # restrict all pages that are not in apps API, healthcheck, admin, etc
     RESTRICTED_APP_NAMES.append('')
-
