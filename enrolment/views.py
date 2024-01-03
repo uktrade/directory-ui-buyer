0351c11c81f1ba7713976c690897298c32c8907a
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-01-03 16:17:51 +0000

    test

--- enrolment/views.py
+++ enrolment/views.py
@@ -6,6 +6,7 @@ from directory_components.helpers import add_next
 from django.conf import settings
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
+from directory_sso_api_client.backends import SSOUserBackend
 
 
 class DomesticLandingView(TemplateView):
@@ -24,7 +25,11 @@ class DomesticLandingView(TemplateView):
                 destination_url=settings.SSO_PROXY_LOGIN_URL,
                 current_url=quote(enrolment_url)
             )
+        backend = SSOUserBackend()
+        user = backend.authenticate(self.request)
         return super().get_context_data(
             **kwargs,
-            enrolment_url=enrolment_url
+            enrolment_url=enrolment_url,
+            session_id=self.request.COOKIES.get(settings.SSO_SESSION_COOKIE),
+            user=user,
         )
