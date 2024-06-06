317b40470b2acbf98e512f804817dd470b57ad97
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-06-06 11:10:59 +0100

    Added two tests

---
+++ core/tests/test_views.py
@@ -0,0 +1,20 @@
+from unittest import mock
+
+from django.urls import reverse
+
+from core.pingdom.services import RedisHealthCheck
+
+
+def test_pingdom_redis_healthcheck_ok(client):
+    response = client.get(reverse('pingdom'))
+    assert response.status_code == 200
+
+
+@mock.patch.object(RedisHealthCheck, 'check')
+def test_pingdom_redis_healthcheck_false(mock_redis_check, client):
+    mock_redis_check.return_value = (
+        False,
+        'Database Error',
+    )           
+    response = client.get(reverse('pingdom'))
+    assert response.status_code == 500
